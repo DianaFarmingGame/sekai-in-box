2c8cfaa3def03bda982193c27e692e102e62d10f
基尘 <jichenssg@gmail.com>
2024-01-13 17:06:41 +1100

    FIX: 删除Item未完全删除字段

--- test/gss/configurator/item.gsm.gd
+++ test/gss/configurator/item.gsm.gd
@@ -8,7 +8,7 @@ func gsm():
 	defunc (get_base :gd """, get_base,""")
 
 	csv/map-let(+(*config_base* "item.csv")
-		[ref ID 名称 描述 组 能否取得 能否堆叠 堆叠上限 能否修复 最高可用度 主要行为消耗 次要行为的消耗 能否交易 价格 保值度 图片/图标]
+		[ref ID 名称 描述 组 能否堆叠 堆叠上限 能否修复 最高可用度 主要行为消耗 次要行为的消耗 能否交易 价格 保值度 图片/图标]
 
 		define(get_base(组)
 			ref num(ref)
