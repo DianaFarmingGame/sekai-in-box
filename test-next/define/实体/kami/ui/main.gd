f8186ce92af5e18cb457490fa298a2691096b341
Lane Sun <lanesun@anlbrain.com>
2024-02-07 21:55:46 +0800

    修改神大人的话语

--- test-next/define/实体/kami/ui/main.gd
+++ test-next/define/实体/kami/ui/main.gd
@@ -1,9 +1,15 @@
 extends Control
 
+const tags := [
+	"神様思考中...",
+	"神様俯视中...",
+	"神様犹豫中...",
+	"神様观察中...",
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	%Tag.text = tags[randi_range(0, tags.size() - 1)]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
