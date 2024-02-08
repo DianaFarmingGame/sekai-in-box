1b081fc2ecfd649bbec7f34023f9a63302882e03
jichen <jichenssg@gmail.com>
2024-02-08 19:55:28 +1100

    add error handle

--- lib/sekai/traits/t_database.gd
+++ lib/sekai/traits/t_database.gd
@@ -8,7 +8,9 @@ var props := {
 
 	&"db/set": func(ctx: LisperContext, this: Mono, key: StringName, value, group: StringName = &"default") -> void:
 		var db = this.getp("data") as Dictionary
-		db[group] = db[group] if db.has(group) else {}
+		if !db.has(group):
+			db[group] =  {}
+			print("db/set: ", "create database group: ", group)
 		db[group][key] = value
 		,
 
@@ -16,42 +18,54 @@ var props := {
 		var db = this.getp("data") as Dictionary
 		if db.has(group) and db[group].has(key):
 			return db[group][key]
-		else:
-			return null
+		
+		push_error("db/get: ", group, " : ", key, " not exist")
+		return null
 		,
 
 	&"db/getp": func(ctx: LisperContext, this: Mono, key: StringName, props_: StringName, group: StringName = &"default") -> Variant:
 		var db = this.getp("data") as Dictionary
 		if db.has(group) and db[group].has(key):
 			var data = db[group][key]
-			assert(data is Dictionary)
-			return data[props_] if data.has(props_) else null
+			if !(data is Dictionary):
+				push_error("db/getp: ", group, " : ", key ," is not Dictionary")
+				return null
+			
+			if data.has(props_):
+				return data[props_]
+
+			push_error("db/getp: ", group, " : ", key, " : ", props_ ," not exist")
 		else:
-			return null
+			push_error("db/getp: ", group, " : ", key, " not exist")
+
+		return null
 		,
 
-	&"db/setp": func(ctx: LisperContext, this: Mono, key: StringName, props_: StringName, value, group: StringName = &"default") -> void:
+	&"db/setp": func(ctx: LisperContext, this: Mono, key: StringName, props_: StringName, value, group: StringName = &"default"):
 		var db = this.getp("data") as Dictionary
 		if db.has(group) and db[group].has(key):
 			var data = db[group][key]
-			assert(data is Dictionary)
+			if !(data is Dictionary):
+				push_error("db/setp: ", group, " : ", key ," is not Dictionary")
+				return
+			
 			data[props_] = value
 		else:
-			db[group] = db[group] if db.has(group) else {}
-			db[group][key] = {props_: value}
+			push_warning("db/setp: ", group, " : ", key, " not exist")
+			this.applym(ctx, &"db/set", [key, value, {props_: value}])
 		,
 
 	&"db/set_group_info": func(ctx: LisperContext, this: Mono, group: StringName, mapping: Dictionary):
 		var gi = this.getp("group_info")
 		if gi.has(group):
-			print("Repeat define group_info ", group)
+			push_warning(&"db/set_group_info: ", "define existing group_info ", group)
 		gi[group] = mapping
 		,
 
 	&"db/get_group_info": func(ctx: LisperContext, this: Mono, group: StringName, mapping: Dictionary) -> Dictionary:
 		var gi = this.getp("group_info")
 		if !gi.has(group) || group == &"default":
-			print("missing group_info ", group)
+			push_warning("db/get_group_info: ", "missing group_info ", group)
 			return {}
 		return gi[group]
 }
