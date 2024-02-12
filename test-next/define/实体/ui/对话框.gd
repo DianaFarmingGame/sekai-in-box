c1b78323eb8bccd0fc10b562eee7758c2834079e
Lane Sun <lanesun@anlbrain.com>
2024-02-12 22:33:07 +0800

    为对话框 UI 添加立绘槽位

--- test-next/define/实体/ui/对话框.gd
+++ test-next/define/实体/ui/对话框.gd
@@ -4,8 +4,16 @@ var id := &"UI对话框"
 var requires := [&"ui"]
 
 var props := {
+	#
+	# 方法
+	#
+	
+	# 弹出一个对话并等待其结束
+	# @params: SekaiControl, {text: String, name?: String, avatar?: Texture2D}
 	&"dialog/put": func (ctx: LisperContext, this: Mono, ctrl: SekaiControl, param: Dictionary) -> void:
 		await this.applymRSU(ctx, &"ui/oneshot", [ctrl, &"dialog", param]),
+	
+	
 	
 	#--------------------------------------------------------------------------#
 	&"ui_data": {
