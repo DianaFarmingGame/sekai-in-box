a3aae7175190cb5e8cba72ca15037ea18f866438
tuzhanhong <tuzhanhong@corp.netease.com>
2024-03-04 22:16:31 +0800

    任务奖励

--- test-next/define/实体/ui/任务栏/task.gd
+++ test-next/define/实体/ui/任务栏/task.gd
@@ -4,6 +4,8 @@ var this: Mono
 var context: LisperContext
 var control: SekaiControl
 
+const Slot = preload("res://test-next/define/实体/ui/合成台/slot.tscn")
+
 func _ready() -> void:
 	draw_task()
 
@@ -21,11 +23,24 @@ func draw_task():
 	for task in tasks.values():
 		var name = task["data"]["name"]
 		var desc = task["data"]["desc"]
+		var rewards = task["data"]["rewards"]
 		var node = TaskButton.new()
 		node.text = name
 		node.desc = desc
+		node.rewards = rewards
 		node.connect("press", _on_button_pressed)
 		%CraftList.add_child(node)
 		
-func _on_button_pressed(desc: String):
+func _on_button_pressed(desc: String, rewards: Dictionary):
 	$describe.text = desc
+	for child in $HBoxContainer.get_children():
+		child.queue_free()
+	for id in rewards:
+		if id == "money":
+			continue
+		var item = sekai.get_define(id)
+		var slot = Slot.instantiate()
+		slot.item_texture = item.get_prop(&"asserts")[&"icon"]
+		slot.label = str(rewards[id])
+		$HBoxContainer.add_child(slot)
+		
