258f0778c190837d6d21721a0a59f8fe20b0209e
Lane Sun <lanesun@anlbrain.com>
2024-03-06 17:31:25 +0800

    诸多修复

--- test-next/define/环境/block_static_ext.gd
+++
@@ -1,7 +0,0 @@
-class_name GBlockStaticExt extends GBlockStatic
-
-func do_merge(sets: Array[Dictionary]) -> Array[Dictionary]:
-	super.do_merge(sets)
-	name = "GBlockStaticExt"
-	merge_traits(sets, [TCompile, TRandom, TATile, TADestroy])
-	return sets
--- test-next/define/环境/装饰/栅栏.gd
+++ test-next/define/环境/装饰/栅栏.gd
@@ -4,6 +4,4 @@ func do_merge(sets: Array[Dictionary]) -> Array[Dictionary]:
 	super.do_merge(sets)
 	name = "栅栏"
 	merge_traits(sets, [TCompile, TDrawStatic, TATile])
-	merge_props(sets, {
-	})
 	return sets
