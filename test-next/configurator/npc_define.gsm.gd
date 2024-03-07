48687b0e7235aa2a489130b2bcf5b6ac03727eae
基尘 <jichenssg@gmail.com>
2024-03-08 10:16:01 +1100

    uid

--- test-next/configurator/npc_define.gsm.gd
+++ test-next/configurator/npc_define.gsm.gd
@@ -5,8 +5,9 @@ csv/map-let(+(*config_base* "npc.csv")
 	[ID ref 名称 位置X 位置Y 位置Z 图片/站立 图片/移动 图片/头像 行为]
 	define(load ("../define/实体/npc.gd")
 		ref num(ref)
-		id 	ID
+		id 	keyword(ID)
 		props {
+			uid keyword(ID)
 			solid_box rect2(-0.3 -0.1 0.6 0.2)
 			name 名称
 			asserts {
